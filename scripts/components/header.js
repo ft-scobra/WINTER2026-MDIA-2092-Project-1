7a6583498d9b3bd576a8580cd952843f54b45b62
ft-scobra <stansmail02@gmail.com>
2026-02-17 02:57:21 -0800

    Added more header options.

--- scripts/components/header.js
+++ scripts/components/header.js
@@ -6,13 +6,30 @@ class Header extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
     <header class="header">
-        <nav>
-            <a href="#">Home</a>
-            <a href="#">Link</a>
-            <a href="#">Link</a>
-            <a href="#">Link</a>
-            <a href="#">Link</a>
-        </nav>
+        <div class="header-logo">
+            <a href="#">
+                <img src="../images/placeholder-image.webp" alt="header logo" class="header-logo-img">
+            </a>
+        </div>
+        <div class="header-nav">
+            <a href="./webpages/home.html" class="header-nav-link">Home</a>
+            <a href="#" class="header-nav-link">Link</a>
+            <a href="#" class="header-nav-link">Link</a>
+            <a href="#" class="header-nav-link">Link</a>
+            <a href="./webpages/about.html" class="header-nav-link">About</a>
+        </div>
+        <div class="header-user">
+            <div class="cart">
+                <img src="../images/icons/shopping-cart-icon.svg" alt="shopping cart button" class="icon cart-icon">
+            </div>
+            <div class="user-account">
+                <img src="../images/icons/account-icon.svg" alt="user account button" class="icon account-icon">
+            </div>
+            <div class="search">
+                <input type="text" placeholder="Search" class="search-textbox">
+                <img src="../images/icons/search-icon.svg" alt="search button" class="icon search-icon">
+            </div>
+        </div>
     </header>
     `
     }
