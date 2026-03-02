dfe190780e7192a117a1a2398371cc283fdde929
Stanislav Ferrao <64654942+ft-scobra@users.noreply.github.com>
2026-03-02 03:40:03 -0800

    Added TTC Page

--- scripts/components/ttc-stickers-bg.js
+++ scripts/components/ttc-stickers-bg.js
@@ -7,9 +7,9 @@ class div extends HTMLElement {
         this.innerHTML = `
     <!-- BG Stickers -->
         <div class="stickers-bg">
-            <img src="../images/stickers/ttc-sticker1.png" alt="" class="sticker1">
+            <img src="../images/stickers/ttc-sticker1.svg" alt="" class="sticker1">
             <img src="../images/stickers/ttc-sticker2.png" alt="" class="sticker2">
-            <img src="../images/stickers/ttc-sticker3.png" alt="" class="sticker3">
+            <img src="../images/stickers/ttc-sticker3.svg" alt="" class="sticker3">
         </div>
     `
     }
